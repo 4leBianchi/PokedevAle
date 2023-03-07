fe217850efe05afe4a1f4779f1d1b2c6e40c3f4c
hastega <hastega@MacBook-Air-di-hastega.local>
2023-03-07 12:24:02 +0100

    creazione componente pokemon-detail e suo path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonDetailComponent } from './ui/pokemon-detail/pokemon-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PokemonDetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
